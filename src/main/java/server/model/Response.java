b6815ac392a96af267d651777a81fb8a68e6a2ee
Mihhail Sokolov <m.sokolov@student.tudelft.nl>
2019-02-19 12:12:55 +0100

    Basic server

---
+++ src/main/java/server/model/Response.java
@@ -0,0 +1,13 @@
+package server.model;
+
+public class Response {
+    private String message;
+
+    public Response(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
