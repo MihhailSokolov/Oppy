31a9d437199fc34afd4d170ecd6ef80222e09d73
Mihhail Sokolov <m.sokolov@student.tudelft.nl>
2019-02-26 10:50:58 +0100

    Added DB controller class which will contain methods for querying db

---
+++ src/main/java/server/db/DbDataController.java
@@ -0,0 +1,10 @@
+package server.db;
+
+// Class which will contain all the methods for querying the database
+public class DbDataController {
+    // Check if provided user details are correct
+    public static boolean isUserAuthenticated(String username, String pass){
+        // Yet to be implemented
+        return true;
+    }
+}
