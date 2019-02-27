eed6e977778e0eed7acb7a297897ba0183b67e22
Benjamin <B.T.Knol@student.tudelft.nl>
2019-02-27 18:24:43 +0100

    Layout for the login page should be done

--- src/main/java/UI/LoginPage.java
+++ src/main/java/UI/LoginPage.java
@@ -1,5 +1,6 @@
-package main.java.UI;
+package UI;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -17,15 +18,32 @@ public class LoginPage extends Application {
     public void start(Stage primaryStage) throws Exception {
         Stage window = primaryStage;
         window.setTitle("LoginPage");
-        Button button = new Button("Login");
-        Label username = new Label("Username/email");
-        Label password = new Label("Password");
-        TextField usernameTextfield = new TextField();
-        TextField passwordTextfield = new TextField();
 
         GridPane layout = new GridPane();
-        layout.getChildren().addAll(button, username, password, usernameTextfield, passwordTextfield);
-        Scene scene = new Scene(layout, 800, 600);
+        layout.setPadding(new Insets(10, 10, 10, 10));
+        layout.setVgap(8);
+        layout.setHgap(10);
+
+        Button loginButton = new Button("Login");
+        GridPane.setConstraints(loginButton, 2, 2);
+
+        Label username = new Label("Username/email");
+        GridPane.setConstraints(username, 0, 0);
+
+        Label password = new Label("Password");
+        GridPane.setConstraints(password, 0, 1);
+
+        TextField usernameTextfield = new TextField();
+        usernameTextfield.setPromptText("Username");
+        GridPane.setConstraints(usernameTextfield, 1, 0);
+
+        TextField passwordTextfield = new TextField();
+        passwordTextfield.setPromptText("Password");
+        GridPane.setConstraints(passwordTextfield, 1, 1);
+
+        layout.getChildren().addAll(loginButton, username, password, usernameTextfield, passwordTextfield);
+
+        Scene scene = new Scene(layout, 400, 300);
 
         window.setScene(scene);
         window.show();
