228925881b5ab84ad246298d7d9025361f939a23
Duyemo <45041539+Duyemo@users.noreply.github.com>
2019-03-01 13:42:33 +0100

    unnecessary inports deleted

--- src/main/java/UI/LoginPage.java
+++ src/main/java/UI/LoginPage.java
@@ -1,6 +1,5 @@
 package UI;
 
-import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
--- src/main/java/UI/Main.java
+++ src/main/java/UI/Main.java
@@ -1,13 +1,6 @@
 package UI;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
