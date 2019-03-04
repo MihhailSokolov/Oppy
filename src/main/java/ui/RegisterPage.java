e868b99c70ba6b85bceb83d41e3e544fe2d0341a
stdcout42 <sammm.sh@gmail.com>
2019-03-04 11:04:32 +0100

    Fix checkstyle for UI side - add checkstyle report screenshot

--- src/main/java/ui/RegisterPage.java
+++ src/main/java/ui/RegisterPage.java
@@ -1,17 +1,26 @@
-package UI;
+package ui;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class RegisterPage {
-
-    public static Scene registerScene(Stage primaryStage){
+    /**
+     * Register page.
+     * @param primaryStage primStage
+     * @return scene.
+     */
+    public static Scene registerScene(Stage primaryStage) {
         Stage window = primaryStage;
         window.setTitle("RegisterPage");
 
@@ -26,24 +35,24 @@ public class RegisterPage {
         GridPane.setConstraints(displayLogo, 0, 0, 3, 1);
 
         //fake register button at the top
-        ToggleGroup loginRegister = new ToggleGroup();
         ToggleButton fakeRegisterButton = new ToggleButton("Register");
         fakeRegisterButton.setSelected(true);
         fakeRegisterButton.setDisable(true);
         GridPane.setConstraints(fakeRegisterButton, 1, 1);
+        ToggleGroup loginRegister = new ToggleGroup();
         fakeRegisterButton.setToggleGroup(loginRegister);
 
         //The button the redirects to the login page
         ToggleButton loginButton = new ToggleButton("Sign in");
         GridPane.setConstraints(loginButton, 0, 1);
-        loginButton.setOnAction(e ->{
-            window.setScene(LoginPage.LoginScene(window));
+        loginButton.setOnAction(e -> {
+            window.setScene(LoginPage.loginScene(window));
         });
         loginButton.setToggleGroup(loginRegister);
 
         //email, username, password  and confirm password fields and labels
         Label email = new Label("email");
-        GridPane.setConstraints(email, 0,2);
+        GridPane.setConstraints(email, 0, 2);
 
         Label username = new Label("Username");
         GridPane.setConstraints(username, 0, 3);
@@ -52,7 +61,7 @@ public class RegisterPage {
         GridPane.setConstraints(password, 0, 4);
 
         Label confirmPassword = new Label("Confirm password");
-        GridPane.setConstraints(confirmPassword,0,5);
+        GridPane.setConstraints(confirmPassword, 0, 5);
 
         TextField emailTextfield = new TextField();
         emailTextfield.setPromptText("email");
@@ -68,16 +77,19 @@ public class RegisterPage {
 
         PasswordField confirmPasswordTextfield = new PasswordField();
         confirmPasswordTextfield.setPromptText("Confirm password");
-        GridPane.setConstraints(confirmPasswordTextfield,1,5);
+        GridPane.setConstraints(confirmPasswordTextfield, 1, 5);
 
 
         //Here the register button is created
         Button registerButton = new Button("Register");
-//      registerButton.setOnAction(to be done); this will check if your email and username are unique and if they are create your account in the database
+        //registerButton.setOnAction(to be done); this will check if
+        // your email and username are unique and if they are create your account in the database
         GridPane.setConstraints(registerButton, 2, 6);
 
         //Here all elements previously created are added to the vieuw and the vieuw is centerd
-        grid.getChildren().addAll(email, username, password, confirmPassword, emailTextfield, usernameTextfield, passwordTextfield, confirmPasswordTextfield, registerButton, loginButton, fakeRegisterButton, displayLogo);
+        grid.getChildren().addAll(email, username, password, confirmPassword, emailTextfield,
+                usernameTextfield, passwordTextfield, confirmPasswordTextfield,
+                registerButton, loginButton, fakeRegisterButton, displayLogo);
         grid.setAlignment(Pos.CENTER);
 
         //here the create vieuw is made into a scene and returnd when the method is called
