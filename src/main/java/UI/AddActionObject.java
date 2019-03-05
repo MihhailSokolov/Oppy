5c8fe547cf89a0a6edc88d70f556f1caf4f55dd2
Duyemo <45041539+Duyemo@users.noreply.github.com>
2019-03-05 20:16:24 +0100

    Created an object for the actions, will be moved out of this map later

---
+++ src/main/java/UI/AddActionObject.java
@@ -0,0 +1,16 @@
+package UI;
+
+public class AddActionObject {
+    public String nameAction;
+    public String category;
+    public int points;
+    public boolean selected;
+
+
+    public AddActionObject(String nameAction, String category, int points){
+        this.nameAction = nameAction;
+        this.category =category;
+        this.points = points;
+        this.selected = false;
+    }
+}
