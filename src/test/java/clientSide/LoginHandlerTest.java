f8879a41260f7456a7a68dc7a56781696e09b1f8
Benjamin <B.T.Knol@student.tudelft.nl>
2019-03-14 14:07:37 +0100

    add stump for request test

--- src/test/java/clientSide/LoginHandlerTest.java
+++ src/test/java/clientSide/LoginHandlerTest.java
@@ -31,4 +31,9 @@ public class LoginHandlerTest {
                 "&pass=0d6be69b264717f2dd33652e212b173104b4a647b7c11ae72e9885f11cd312fb",
                 result);
     }
+
+    @Test
+    public void sendLoginTest(){
+        
+    }
 }
