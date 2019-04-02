b3f7d226fae6158f25b29d6471eadc07df7cf61b
Duyemo <45041539+Duyemo@users.noreply.github.com>
2019-04-02 13:42:49 +0200

    start fix

--- src/main/java/ui/AddActionPage.java
+++ src/main/java/ui/AddActionPage.java
@@ -1,7 +1,12 @@
 package ui;
 
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
