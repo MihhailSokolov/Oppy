ae633e1cf7c400f52b57b5a0e2b370da1f01e312
aaaa <o.ledelay@student.tudelft.nl>
2019-04-06 13:34:10 +0200

    split equals test and resolve warnings

--- src/test/java/server/model/ActionTest.java
+++ src/test/java/server/model/ActionTest.java
@@ -1,5 +1,6 @@
 package server.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Date;
@@ -7,6 +8,24 @@ import java.util.Date;
 import static org.junit.Assert.*;
 
 public class ActionTest {
+
+    private Action action1;
+    private Action action2;
+    private Action action3;
+    private Action action4;
+    private Action action5;
+    private Action action6;
+
+
+    @Before
+    public void setUp() {
+        action1 = new Action("recycle bottle","recycling",10);
+        action2 = new Action("recycle bottle","recycling",10);
+        action3 = new Action("vegetable meal","food",15);
+        action4 = new Action("recycle bottle","food",10);
+        action5 = new Action("vegetable meal","recycling",10);
+        action6 = new Action("recycle bottle","recycling",15);
+    }
 
     @Test
     public void constructorTest() {
@@ -59,23 +78,49 @@ public class ActionTest {
         assertEquals("Action[actionName='recycle bottle', category='recycling', points='10']", action.toString());
     }
 
-    @SuppressWarnings("all")
     @Test
-    public void testEquals() {
-        Action action1 = new Action("recycle bottle","recycling",10);
-        Action action2 = new Action("recycle bottle","recycling",10);
-        Action action3 = new Action("vegetable meal","food",15);
-        Action action4 = new Action("recycle bottle","food",10);
-        Action action5 = new Action("vegetable meal","recycling",10);
-        Action action6 = new Action("recycle bottle","recycling",15);
-        assertTrue(action1.equals(action2));
-        assertTrue(action1.equals(action1));
-        assertFalse(action1.equals(action3));
-        assertFalse(action1.equals(action4));
-        assertFalse(action1.equals(action5));
-        assertFalse(action1.equals(action6));
-        assertFalse(action1.equals(null));
-        assertFalse(action1.equals("String test"));
-        assertFalse(action1.equals(new User("user", "pass", "mail", 10, new Date())));
+    public void testEqualsSimilar() {
+        assertEquals("An action should be equal to another action with the same values for fields.", action1, action2);
     }
+
+    @Test
+    public void testEqualsSelf() {
+        assertEquals("An action should be equal to itself.", action1, action1);
+    }
+
+    @Test
+    public void testEqualsCompletelyDifferent() {
+        assertNotEquals("An action with different field values should not be equal.", action1, action3);
+    }
+
+    @Test
+    public void testEqualsCategoryDifferent() {
+        assertNotEquals("If an action is in a different category, it should not be equal.", action1, action4);
+    }
+
+    @Test
+    public void testEqualsNameDifferent() {
+        assertNotEquals("If an action has a different name, it should not be equal.", action1, action5);
+    }
+
+    @Test
+    public void testEqualsPointsDifferent() {
+        assertNotEquals("If an action has a different number of points, it should not be equal.", action1, action6);
+    }
+
+    @Test
+    public void testEqualsNotNull() {
+        assertNotEquals("An action should not be equal to null.", action1, null);
+    }
+
+    @Test
+    public void testEqualsNotString() {
+        assertNotEquals("An action should not be equal to a String.", action1, "String");
+    }
+
+    @Test
+    public void testEqualsNotUser() {
+        assertNotEquals("An action should not be equal to a User.", action1, new User("user", "pass", "mail", 10, new Date()));
+    }
+
 }
