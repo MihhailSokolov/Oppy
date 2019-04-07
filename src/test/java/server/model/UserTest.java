0ffdf284e05a75f5b5b9211df9b86a2473298a3d
aaaa <o.ledelay@student.tudelft.nl>
2019-04-07 22:00:43 +0200

    split and improve UserTest

--- src/test/java/server/model/UserTest.java
+++ src/test/java/server/model/UserTest.java
@@ -1,8 +1,8 @@
 package server.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -12,34 +12,62 @@ import static org.junit.Assert.*;
 
 public class UserTest {
 
+    private User user1;
+    private User user2;
+    private User user3;
+    private User user4;
+    private User user5;
+    private User user6;
+    private User user7;
+    private User user8;
+    private User user9;
+    private User user10;
+    private User user11;
+    private User user12;
+
+    @Before
+    public void setUp(){
+        user1 = new User("hugo","pass","test@gmail.com",100, new Date());
+        user2 = new User("hugo","pass","test@gmail.com",100, new Date());
+        user3 = new User("nothugo","differentpass","mail@gmail.com",200, new Date(0));
+        user4 = new User("nothugo","pass","test@gmail.com",100, new Date());
+        user5 = new User("hugo","differentpass","test@gmail.com",100, new Date());
+        user6 = new User("hugo","pass","mail@gmail.com",100, new Date());
+        user7 = new User("hugo","pass","test@gmail.com",200, new Date());
+        user8 = new User("hugo","pass","test@gmail.com",100, new Date(0));
+        user9 = new User("hugo","pass","test@gmail.com",100, new Date());
+        user9.setAnonymous(true);
+        user10 = new User("hugo","pass","test@gmail.com",100, new Date());
+        user10.setProfilePicture("1010100101010010111110");
+        user11 = new User("hugo","pass","test@gmail.com",100, new Date());
+        user11.setFriends(Arrays.asList(user5, user6, user7));
+        user12 = new User("hugo","pass","test@gmail.com",100, new Date());
+        user12.setPresets(Arrays.asList(new Preset("preset1", new ArrayList<>()), new Preset("preset2", new ArrayList<>())));
+    }
+
     @Test
     public void constructorTest() {
-        User u = new User("hugo","pass","test@gmail.com",100, new Date());
-        assertNotNull(u);
+        assertNotNull(user1);
     }
 
     @Test
     public void getUsername() {
-        User u = new User("hugo","pass","test@gmail.com",100, new Date());
-        assertEquals("hugo",u.getUsername());
+        assertEquals("hugo", user1.getUsername());
     }
 
     @Test
     public void getPassword() {
-        User u = new User("hugo","pass","test@gmail.com",100, new Date());
-        assertEquals("pass",u.getPassword());
+        assertEquals("pass", user1.getPassword());
     }
 
     @Test
     public void getEmail() {
-        User u = new User("hugo","pass","test@gmail.com",100, new Date());
-        assertEquals("test@gmail.com",u.getEmail());
+        assertEquals("test@gmail.com", user1.getEmail());
     }
 
     @Test
     public void getScore() {
-        User u = new User("hugo","pass","test@gmail.com",100, new Date());
-        assertEquals(100,u.getScore());
+        assertEquals(100, user1.getScore());
     }
 
     @Test
@@ -79,54 +107,12 @@ public class UserTest {
 
     @Test
     public void getRegisterDate() {
-        User u = new User("hugo","pass","test@gmail.com",100, new Date());
-        assertEquals(new Date(), u.getRegisterDate());
-    }
-
-    @SuppressWarnings("all")
-    @Test
-    public void testEquals() {
-        User user1 = new User("hugo","pass","test@gmail.com",100, new Date());
-        User user2 = new User("hugo","pass","test@gmail.com",100, new Date());
-        User user3 = new User("nothugo","differentpass","mail@gmail.com",200, new Date());
-        User user4 = new User("hugo","differentpass","test@gmail.com",100, new Date());
-        User user5 = new User("hugo","pass","mail@gmail.com",100, new Date());
-        User user6 = new User("hugo","pass","test@gmail.com",200, new Date());
-        User user7 = new User("hugo","pass","test@gmail.com",100, new Date(0));
-        User user8 = new User("nothugo","diffpass","test@gmail.com",100, new Date());
-        User user9 = new User("nothugo","pass","nottest@gmail.com",100, new Date());
-        User user10 = new User("nothugo","pass","test@gmail.com",100, new Date(0));
-        User user11 = new User("hugo","pass","test@gmail.com",100, new Date());
-        user11.setAnonymous(true);
-        User user12 = new User("hugo","pass","test@gmail.com",100, new Date());
-        user12.setProfilePicture("1010100101010010111110");
-        User user13 = new User("hugo","pass","test@gmail.com",100, new Date());
-        user13.setFriends(Arrays.asList(user5, user6, user7));
-        User user14 = new User("hugo","pass","test@gmail.com",100, new Date());
-        user14.setPresets(Arrays.asList(new Preset("preset1", new ArrayList<>()), new Preset("preset2", new ArrayList<>())));
-        assertTrue(user1.equals(user2));
-        assertTrue(user1.equals(user1));
-        assertFalse(user1.equals(user3));
-        assertFalse(user1.equals(user4));
-        assertFalse(user1.equals(user5));
-        assertFalse(user1.equals(user6));
-        assertFalse(user1.equals(user7));
-        assertFalse(user1.equals(user8));
-        assertFalse(user1.equals(user9));
-        assertFalse(user1.equals(user10));
-        assertFalse(user1.equals(user11));
-        assertFalse(user1.equals(user12));
-        assertFalse(user1.equals(user13));
-        assertFalse(user1.equals(user14));
-        assertFalse(user1.equals(null));
-        assertFalse(user1.equals("String test"));
-        assertFalse(user1.equals(new Action("name", "category", 10)));
+        assertEquals(new Date(), user1.getRegisterDate());
     }
 
     @Test
     public void getPresets() {
-        User user = new User("hugo","pass","test@gmail.com",100, new Date());
-        assertNotNull(user.getPresets());
+        assertNotNull(user1.getPresets());
     }
 
     @Test
@@ -137,5 +123,80 @@ public class UserTest {
         presets.add(preset);
         user.setPresets(presets);
         assertEquals(presets, user.getPresets());
+    }
+    
+    @Test
+    public void testEqualsSimilar() {
+        assertEquals("A User should be equal to another User with the same values for fields.", user1, user2);
+    }
+
+    @Test
+    public void testEqualsSelf() {
+        assertEquals("A User should be equal to itself.", user1, user1);
+    }
+
+    @Test
+    public void testEqualsCompletelyDifferent() {
+        assertNotEquals("A User with different field values should not be equal.", user1, user3);
+    }
+
+    @Test
+    public void testEqualsNameDifferent() {
+        assertNotEquals("If a User has a different name, it should not be equal.", user1, user4);
+    }
+
+    @Test
+    public void testEqualsPasswordDifferent() {
+        assertNotEquals("If a User has a different password, it should not be equal.", user1, user5);
+    }
+
+    @Test
+    public void testEqualsEmailDifferent() {
+        assertNotEquals("If a User has a different email, it should not be equal.", user1, user6);
+    }
+
+    @Test
+    public void testEqualsScoreDifferent() {
+        assertNotEquals("If a User has a different score, it should not be equal.", user1, user7);
+    }
+
+    @Test
+    public void testEqualsDateDifferent() {
+        assertNotEquals("If a User has a different creation date, it should not be equal.", user1, user8);
+    }
+
+    @Test
+    public void testEqualsAnonDifferent() {
+        assertNotEquals("If a User has a different anonymity state, it should not be equal.", user1, user9);
+    }
+
+    @Test
+    public void testEqualsPicDifferent() {
+        assertNotEquals("If a User has a different profile picture, it should not be equal.", user1, user10);
+    }
+
+    @Test
+    public void testEqualsFriendsDifferent() {
+        assertNotEquals("If a User has different friends, it should not be equal.", user1, user11);
+    }
+
+    @Test
+    public void testEqualsPresetsDifferent() {
+        assertNotEquals("If a User has a different presets, it should not be equal.", user1, user12);
+    }
+
+    @Test
+    public void testEqualsNotNull() {
+        assertNotEquals("A User should not be equal to null.", user1, null);
+    }
+
+    @Test
+    public void testEqualsNotString() {
+        assertNotEquals("A User should not be equal to a String.", user1, "String");
+    }
+
+    @Test
+    public void testEqualsNotUser() {
+        assertNotEquals("A User should not be equal to an Action.", user1, new Action("name", "category", 10));
     }
 }
